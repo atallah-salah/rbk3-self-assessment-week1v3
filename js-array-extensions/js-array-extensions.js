d408f2a113831046ee7779c21782fac04e4dc1da
atallah-salah <atallah_salah@outlook.com>
2017-08-26 10:06:58 +0300

    solved array q

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,12 @@
-// your code here
+var Array=function(array){
+    this.array=array;
+    this.first=first
+    this.last=last
+
+}
+var first=function(){
+    return  this.array[0]
+  };
+var last=  function(){
+    return  this.array[ this.array.length]
+  };
