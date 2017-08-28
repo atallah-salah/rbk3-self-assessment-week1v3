412fc3a0a63b01d5949ee49e0a7ba02c526d4217
atallah-salah <atallah_salah@outlook.com>
2017-08-29 01:06:03 +0300

    remove counter

--- ds-stack/ds-stack.js
+++ ds-stack/ds-stack.js
@@ -1,16 +1,16 @@
 var Stack = function() {
     this._storage =[];
-    this.counter=0;
+    
 
 
     this.add = function(){
      this._storage.push(counter)
-     this.counter++;
+     
     };
 
     
     this.remove = function() {
      this._storage.pop()
-     this.counter--;
+     
     };
   };
